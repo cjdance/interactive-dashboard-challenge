0e0fd895ee07ec846aad2f72281bc39d90b6847a
User <c.j.dancewilson@outlook.com>
2021-08-22 10:47:00 +1000

    Updated app.js to populate the dataselector with all the ID's of test subjects and created names for buildPlot and demographic functions.

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,23 @@
-d3.json("data/samples.json").then((data) => {
-    var names = data.names;
-    console.log(names);
-    
-});
+function pageDefault() {    
+    var dataSelect = d3.select("#selDataset");
 
+    d3.json("data/samples.json").then((data) => {
+        var names = data.names;
+        console.log(names);
 
+        dataSelect.selectAll("option")
+            .data(names)
+            .enter()
+            .append("option")
+            .attr("value", d => d)
+            .text(d => d);
+
+        var startUp = names[0];
+
+        buildPlots(startUp);
+        demographics(startUp);
+        
+    }).catch(error => console.log(error));
+};
+
+pageDefault();
